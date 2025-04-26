b5afed1df786c3d535df8f960778172064c1b3db
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2025-04-26 14:35:19 +1000

    Add files via upload

--- config.lua
+++ config.lua
@@ -2,12 +2,24 @@ Config = {}
 
 Config.Recipes = {
     {
-        result = { name = "weed_joint", label = "Joint", count = 1 },
+        result = { name = "watering_can", label = "Watering Can", count = 1 },
         ingredients = {
-            { name = "weed_papers", count = 1 },
-            { name = "weed_budclean", count = 5 }
+            { name = "water_refill", count = 1 },
+            { name = "wateringcan_empty", count = 1 }
         },
-        usableItem = "weed_papers",
+        usableItem = "water_refill",
+        animation = {
+            dict = "missheistfbisetup1",
+            name = "hassle_intro_loop_f",
+            time = 5000
+        }
+    },
+    {
+        result = { name = "rollpaper", label = "Rolling Paper", count = 6 },
+        ingredients = {
+            { name = "rollpapers", count = 1 },
+        },
+        usableItem = "rollpapers",
         animation = {
             dict = "missheistfbisetup1",
             name = "hassle_intro_loop_f",
@@ -15,12 +27,12 @@ Config.Recipes = {
         }
     },
     -- {
-    --     result = { name = "weed_joint", count = 1 },
+    --     result = { name = "weapon_pistol", label = "Pistole", count = 1 },
     --     ingredients = {
-    --         { name = "weed_papers", count = 1 },
-    --         { name = "weed_budclean", count = 5 }
+    --         { name = "steel", count = 1 },
+    --         { name = "wood", count = 5 }
     --     },
-    --     usableItem = "weed_papers",
+    --     usableItem = "steel",
     --     animation = {
     --         dict = "missheistfbisetup1",
     --         name = "hassle_intro_loop_f",
